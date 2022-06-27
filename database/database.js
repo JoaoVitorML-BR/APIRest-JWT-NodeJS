597ca1c383f87699bbfd36447a585055aea81f57
JoaoVitorML-BR <jvmlira33@gmail.com>
2022-06-27 17:06:25 -0300

    Criado rota de Login para cadastro,edição,atualização e leitura via HTML

--- database/database.js
+++ database/database.js
@@ -1,9 +1,9 @@
 // making the database connection
-const sequelize = require('sequelize');
-const connection = new sequelize('apirest', 'root', '1904', {
-    dialect: 'mysql',
-    host: 'localhost',
-    timezone: '-03:00'
-});
+const sequelize = require('sequelize')
+const connection = new sequelize('apirest', 'root', '1234', {
+  dialect: 'mysql',
+  host: 'localhost',
+  timezone: '-03:00'
+})
 
-module.exports = connection;
+module.exports = connection
